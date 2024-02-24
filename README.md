8eb2f1ac80cc00cfab5355cc3eac5f7107129c5d
Mete Ismayil <mismayilza@gmail.com>
2024-02-24 17:37:32 +0100

    add results for order experiments

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ This is the repo for Morphological Compositional Generalization project. For mor
 ## Evaluation Pipeline
 Data is supposed to go through the following pipeline for evaluation:
 
-Raw Data --> | Preprocess Data | --> | Prepare Data for Tasks | --> | Prepare Data for Evaluation | --> | Evaluate Data | --> | Report metrics |
+Raw Data --> | Preprocess Data | --> | Prepare Data for Tasks | --> | Prepare Data for Evaluation | --> | Evaluate Model on Data | --> | Report Metrics |
 
-These steps are done respectively with `preprocess_data.py`, `prepare_data_for_morph.py` and `prepare_data_for_eval.py` scripts. Each pipeline step outputs data along with some metadata. Once the evaluation data is ready, it can be evaluated using the `evaluate_gpt.py` script (e.g. for GPT models) and metrics can be reported using the `report_metrics.py` script. Input data resides in the `data` folder, and outputs of the evaluation reside in `outputs` folder. 
+These steps are done respectively with `preprocess_data.py`, `prepare_data_for_tasks.py` and `prepare_data_for_eval.py` scripts. Each pipeline step outputs data along with some metadata. Once the evaluation data is ready, it can be evaluated using the `evaluate_gpt.py` script (e.g. for GPT models) and metrics can be reported using the `report_metrics.py` script. Input data resides in the `data` folder, and outputs of the evaluation reside in `outputs` folder. 
--- src/evaluate_gpt.sh
+++ src/evaluate_gpt.sh
@@ -2,7 +2,7 @@
 
 openai_api_key=$1
 model=${2:-"gpt-3.5-turbo"}
-version=6
+version=_order
 data_dir="../experiments/data"
 outputs_dir="../experiments/outputs"
 
@@ -12,11 +12,11 @@ do
     python evaluate_gpt.py -d ${jsonfile} -o ${outputs_dir}/${model}/tr/btwd/temp_en/v${version} -k ${openai_api_key} -m ${model}
 done
 
-for jsonfile in ${data_dir}/tr/btwd/temp_tr/v${version}/*.json
-do
-    echo "Evaluating ${jsonfile}"
-    python evaluate_gpt.py -d ${jsonfile} -o ${outputs_dir}/${model}/tr/btwd/temp_tr/v${version} -k ${openai_api_key} -m ${model}
-done
+# for jsonfile in ${data_dir}/tr/btwd/temp_tr/v${version}/*.json
+# do
+#     echo "Evaluating ${jsonfile}"
+#     python evaluate_gpt.py -d ${jsonfile} -o ${outputs_dir}/${model}/tr/btwd/temp_tr/v${version} -k ${openai_api_key} -m ${model}
+# done
 
 for jsonfile in ${data_dir}/en/morpholex/temp_en/v${version}/*.json
 do
@@ -24,11 +24,11 @@ do
     python evaluate_gpt.py -d ${jsonfile} -o ${outputs_dir}/${model}/en/morpholex/temp_en/v${version} -k ${openai_api_key} -m ${model}
 done
 
-for jsonfile in ${data_dir}/en/morpholex/temp_tr/v${version}/*.json
-do
-    echo "Evaluating ${jsonfile}"
-    python evaluate_gpt.py -d ${jsonfile} -o ${outputs_dir}/${model}/en/morpholex/temp_tr/v${version} -k ${openai_api_key} -m ${model}
-done
+# for jsonfile in ${data_dir}/en/morpholex/temp_tr/v${version}/*.json
+# do
+#     echo "Evaluating ${jsonfile}"
+#     python evaluate_gpt.py -d ${jsonfile} -o ${outputs_dir}/${model}/en/morpholex/temp_tr/v${version} -k ${openai_api_key} -m ${model}
+# done
 
 # shots=(1 3 5)
 # for shot in "${shots[@]}"
