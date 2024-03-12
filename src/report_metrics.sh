48ed5c67c7947f6955fc404b199a5efd3513e04b
Mete Ismayil <mismayilza@gmail.com>
2024-03-12 13:22:06 +0100

    add gpt4 metric results for balanced data

--- src/report_metrics.sh
+++ src/report_metrics.sh
@@ -5,5 +5,5 @@ version=_balanced
 
 python report_metrics.py -r ../experiments/outputs/${model}/en/morpholex/temp_en/v${version}
 python report_metrics.py -r ../experiments/outputs/${model}/en/morpholex/temp_tr/v${version}
-# python report_metrics.py -r ../experiments/outputs/${model}/tr/btwd/temp_en/v${version}
-# python report_metrics.py -r ../experiments/outputs/${model}/tr/btwd/temp_tr/v${version}
+python report_metrics.py -r ../experiments/outputs/${model}/tr/btwd/temp_en/v${version}
+python report_metrics.py -r ../experiments/outputs/${model}/tr/btwd/temp_tr/v${version}
