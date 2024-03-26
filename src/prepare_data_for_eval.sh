bced7d6c60f4c936fbd1539933e8941b2c6b1abd
Mete Ismayil <mismayilza@gmail.com>
2024-03-27 00:49:51 +0100

    add sentence experiment metrics and figures

--- src/prepare_data_for_eval.sh
+++ src/prepare_data_for_eval.sh
@@ -1,45 +1,44 @@
 #!/bin/bash
 
-version=_sent
+experiment=_sent
 shots=(1 3 5)
-suffix=""
 data_dir="../experiments/data"
 
 for shot in "${shots[@]}"
 do
     # TR in EN template
-    # python prepare_data_for_eval.py -d ${data_dir}/tr/btwd/btwd_prep_post_balanced_morph_sample${suffix}.json -t morph_gen_en -n ${shot} -o ${data_dir}/tr/btwd/temp_en/v${version} -m
-    # python prepare_data_for_eval.py -d ${data_dir}/tr/btwd/btwd_prep_post_balanced_morph_sample${suffix}.json -t morph_disc_en -n ${shot} -o ${data_dir}/tr/btwd/temp_en/v${version} -m
-    # python prepare_data_for_eval.py -d ${data_dir}/tr/btwd/btwd_prep_post_balanced_morph_sample_nonce${suffix}.json -t morph_gen_en -n ${shot} -o ${data_dir}/tr/btwd/temp_en/v${version} -m
-    # python prepare_data_for_eval.py -d ${data_dir}/tr/btwd/btwd_prep_post_balanced_morph_sample_nonce${suffix}.json -t morph_disc_en -n ${shot} -o ${data_dir}/tr/btwd/temp_en/v${version} -m
+    # python prepare_data_for_eval.py -d ${data_dir}/tr/btwd/btwd_prep_post_balanced_morph_sample.json -t morph_gen_en -n ${shot} -o ${data_dir}/tr/btwd/temp_en/${experiment} -m
+    # python prepare_data_for_eval.py -d ${data_dir}/tr/btwd/btwd_prep_post_balanced_morph_sample.json -t morph_disc_en -n ${shot} -o ${data_dir}/tr/btwd/temp_en/${experiment} -m
+    # python prepare_data_for_eval.py -d ${data_dir}/tr/btwd/btwd_prep_post_balanced_morph_sample_nonce.json -t morph_gen_en -n ${shot} -o ${data_dir}/tr/btwd/temp_en/${experiment} -m
+    # python prepare_data_for_eval.py -d ${data_dir}/tr/btwd/btwd_prep_post_balanced_morph_sample_nonce.json -t morph_disc_en -n ${shot} -o ${data_dir}/tr/btwd/temp_en/${experiment} -m
 
     # # TR in TR template
-    # python prepare_data_for_eval.py -d ${data_dir}/tr/btwd/btwd_prep_post_balanced_morph_sample${suffix}.json -t morph_gen_tr -n ${shot} -o ${data_dir}/tr/btwd/temp_tr/v${version} -m
-    # python prepare_data_for_eval.py -d ${data_dir}/tr/btwd/btwd_prep_post_balanced_morph_sample${suffix}.json -t morph_disc_tr -n ${shot} -o ${data_dir}/tr/btwd/temp_tr/v${version} -m
-    # python prepare_data_for_eval.py -d ${data_dir}/tr/btwd/btwd_prep_post_balanced_morph_sample_nonce${suffix}.json -t morph_gen_tr -n ${shot} -o ${data_dir}/tr/btwd/temp_tr/v${version} -m
-    # python prepare_data_for_eval.py -d ${data_dir}/tr/btwd/btwd_prep_post_balanced_morph_sample_nonce${suffix}.json -t morph_disc_tr -n ${shot} -o ${data_dir}/tr/btwd/temp_tr/v${version} -m
+    # python prepare_data_for_eval.py -d ${data_dir}/tr/btwd/btwd_prep_post_balanced_morph_sample.json -t morph_gen_tr -n ${shot} -o ${data_dir}/tr/btwd/temp_tr/${experiment} -m
+    # python prepare_data_for_eval.py -d ${data_dir}/tr/btwd/btwd_prep_post_balanced_morph_sample.json -t morph_disc_tr -n ${shot} -o ${data_dir}/tr/btwd/temp_tr/${experiment} -m
+    # python prepare_data_for_eval.py -d ${data_dir}/tr/btwd/btwd_prep_post_balanced_morph_sample_nonce.json -t morph_gen_tr -n ${shot} -o ${data_dir}/tr/btwd/temp_tr/${experiment} -m
+    # python prepare_data_for_eval.py -d ${data_dir}/tr/btwd/btwd_prep_post_balanced_morph_sample_nonce.json -t morph_disc_tr -n ${shot} -o ${data_dir}/tr/btwd/temp_tr/${experiment} -m
 
     # TR in EN template for sentence completion
-    python prepare_data_for_eval.py -d ${data_dir}/tr/btwd/balanced/btwd_prep_post_balanced_sample_morph${suffix}.json -t morph_gen_sent_en -n ${shot} -o ${data_dir}/tr/btwd/eval/temp_en/v${version} -m
-    python prepare_data_for_eval.py -d ${data_dir}/tr/btwd/balanced/btwd_prep_post_balanced_sample_morph${suffix}.json -t morph_disc_sent_en -n ${shot} -o ${data_dir}/tr/btwd/eval/temp_en/v${version} -m
-    python prepare_data_for_eval.py -d ${data_dir}/tr/btwd/balanced/btwd_prep_post_balanced_sample_morph_nonce${suffix}.json -t morph_gen_sent_en -n ${shot} -o ${data_dir}/tr/btwd/eval/temp_en/v${version} -m
-    python prepare_data_for_eval.py -d ${data_dir}/tr/btwd/balanced/btwd_prep_post_balanced_sample_morph_nonce${suffix}.json -t morph_disc_sent_en -n ${shot} -o ${data_dir}/tr/btwd/eval/temp_en/v${version} -m
+    python prepare_data_for_eval.py -d ${data_dir}/tr/btwd/balanced/btwd_prep_post_balanced_sample_morph.json -t morph_gen_sent_en -n ${shot} -o ${data_dir}/tr/btwd/eval/temp_en/${experiment} -m
+    python prepare_data_for_eval.py -d ${data_dir}/tr/btwd/balanced/btwd_prep_post_balanced_sample_morph.json -t morph_disc_sent_en -n ${shot} -o ${data_dir}/tr/btwd/eval/temp_en/${experiment} -m
+    python prepare_data_for_eval.py -d ${data_dir}/tr/btwd/balanced/btwd_prep_post_balanced_sample_morph_nonce.json -t morph_gen_sent_en -n ${shot} -o ${data_dir}/tr/btwd/eval/temp_en/${experiment} -m
+    python prepare_data_for_eval.py -d ${data_dir}/tr/btwd/balanced/btwd_prep_post_balanced_sample_morph_nonce.json -t morph_disc_sent_en -n ${shot} -o ${data_dir}/tr/btwd/eval/temp_en/${experiment} -m
 
     # # TR in TR template for sentence completion
-    python prepare_data_for_eval.py -d ${data_dir}/tr/btwd/balanced/btwd_prep_post_balanced_sample_morph${suffix}.json -t morph_gen_sent_tr -n ${shot} -o ${data_dir}/tr/btwd/eval/temp_tr/v${version} -m
-    python prepare_data_for_eval.py -d ${data_dir}/tr/btwd/balanced/btwd_prep_post_balanced_sample_morph${suffix}.json -t morph_disc_sent_tr -n ${shot} -o ${data_dir}/tr/btwd/eval/temp_tr/v${version} -m
-    python prepare_data_for_eval.py -d ${data_dir}/tr/btwd/balanced/btwd_prep_post_balanced_sample_morph_nonce${suffix}.json -t morph_gen_sent_tr -n ${shot} -o ${data_dir}/tr/btwd/eval/temp_tr/v${version} -m
-    python prepare_data_for_eval.py -d ${data_dir}/tr/btwd/balanced/btwd_prep_post_balanced_sample_morph_nonce${suffix}.json -t morph_disc_sent_tr -n ${shot} -o ${data_dir}/tr/btwd/eval/temp_tr/v${version} -m
+    python prepare_data_for_eval.py -d ${data_dir}/tr/btwd/balanced/btwd_prep_post_balanced_sample_morph.json -t morph_gen_sent_tr -n ${shot} -o ${data_dir}/tr/btwd/eval/temp_tr/${experiment} -m
+    python prepare_data_for_eval.py -d ${data_dir}/tr/btwd/balanced/btwd_prep_post_balanced_sample_morph.json -t morph_disc_sent_tr -n ${shot} -o ${data_dir}/tr/btwd/eval/temp_tr/${experiment} -m
+    python prepare_data_for_eval.py -d ${data_dir}/tr/btwd/balanced/btwd_prep_post_balanced_sample_morph_nonce.json -t morph_gen_sent_tr -n ${shot} -o ${data_dir}/tr/btwd/eval/temp_tr/${experiment} -m
+    python prepare_data_for_eval.py -d ${data_dir}/tr/btwd/balanced/btwd_prep_post_balanced_sample_morph_nonce.json -t morph_disc_sent_tr -n ${shot} -o ${data_dir}/tr/btwd/eval/temp_tr/${experiment} -m
 
     # EN in EN template
-    # python prepare_data_for_eval.py -d ${data_dir}/en/morpholex/MorphoLEX_en_prep_balanced_sample_morph${suffix}.json -t morph_gen_en -n ${shot} -o ${data_dir}/en/morpholex/temp_en/v${version} -m
-    # python prepare_data_for_eval.py -d ${data_dir}/en/morpholex/MorphoLEX_en_prep_balanced_sample_morph${suffix}.json -t morph_disc_en -n ${shot} -o ${data_dir}/en/morpholex/temp_en/v${version} -m
-    # python prepare_data_for_eval.py -d ${data_dir}/en/morpholex/MorphoLEX_en_prep_balanced_sample_morph_nonce${suffix}.json -t morph_gen_en -n ${shot} -o ${data_dir}/en/morpholex/temp_en/v${version} -m
-    # python prepare_data_for_eval.py -d ${data_dir}/en/morpholex/MorphoLEX_en_prep_balanced_sample_morph_nonce${suffix}.json -t morph_disc_en -n ${shot} -o ${data_dir}/en/morpholex/temp_en/v${version} -m
+    # python prepare_data_for_eval.py -d ${data_dir}/en/morpholex/MorphoLEX_en_prep_balanced_sample_morph.json -t morph_gen_en -n ${shot} -o ${data_dir}/en/morpholex/temp_en/${experiment} -m
+    # python prepare_data_for_eval.py -d ${data_dir}/en/morpholex/MorphoLEX_en_prep_balanced_sample_morph.json -t morph_disc_en -n ${shot} -o ${data_dir}/en/morpholex/temp_en/${experiment} -m
+    # python prepare_data_for_eval.py -d ${data_dir}/en/morpholex/MorphoLEX_en_prep_balanced_sample_morph_nonce.json -t morph_gen_en -n ${shot} -o ${data_dir}/en/morpholex/temp_en/${experiment} -m
+    # python prepare_data_for_eval.py -d ${data_dir}/en/morpholex/MorphoLEX_en_prep_balanced_sample_morph_nonce.json -t morph_disc_en -n ${shot} -o ${data_dir}/en/morpholex/temp_en/${experiment} -m
     
     # EN in TR template
-    # python prepare_data_for_eval.py -d ${data_dir}/en/morpholex/MorphoLEX_en_prep_balanced_sample_morph${suffix}.json -t morph_gen_tr -n ${shot} -o ${data_dir}/en/morpholex/temp_tr/v${version} -m
-    # python prepare_data_for_eval.py -d ${data_dir}/en/morpholex/MorphoLEX_en_prep_balanced_sample_morph${suffix}.json -t morph_disc_tr -n ${shot} -o ${data_dir}/en/morpholex/temp_tr/v${version} -m
-    # python prepare_data_for_eval.py -d ${data_dir}/en/morpholex/MorphoLEX_en_prep_balanced_sample_morph_nonce${suffix}.json -t morph_gen_tr -n ${shot} -o ${data_dir}/en/morpholex/temp_tr/v${version} -m
-    # python prepare_data_for_eval.py -d ${data_dir}/en/morpholex/MorphoLEX_en_prep_balanced_sample_morph_nonce${suffix}.json -t morph_disc_tr -n ${shot} -o ${data_dir}/en/morpholex/temp_tr/v${version} -m
+    # python prepare_data_for_eval.py -d ${data_dir}/en/morpholex/MorphoLEX_en_prep_balanced_sample_morph.json -t morph_gen_tr -n ${shot} -o ${data_dir}/en/morpholex/temp_tr/${experiment} -m
+    # python prepare_data_for_eval.py -d ${data_dir}/en/morpholex/MorphoLEX_en_prep_balanced_sample_morph.json -t morph_disc_tr -n ${shot} -o ${data_dir}/en/morpholex/temp_tr/${experiment} -m
+    # python prepare_data_for_eval.py -d ${data_dir}/en/morpholex/MorphoLEX_en_prep_balanced_sample_morph_nonce.json -t morph_gen_tr -n ${shot} -o ${data_dir}/en/morpholex/temp_tr/${experiment} -m
+    # python prepare_data_for_eval.py -d ${data_dir}/en/morpholex/MorphoLEX_en_prep_balanced_sample_morph_nonce.json -t morph_disc_tr -n ${shot} -o ${data_dir}/en/morpholex/temp_tr/${experiment} -m
 done
