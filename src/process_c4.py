17aea34a15bc97682e5aa8a540f6cae8264bd537
Mete Ismayilzada <mismayilza@gmail.com>
2024-05-03 23:53:43 +0200

    add c4 processing code

--- src/process_c4.py
+++ src/process_c4.py
@@ -1,149 +1,67 @@
-import argparse
-import multiprocessing
-from datasets import load_dataset
-from tqdm import tqdm
-import spacy
 import pathlib
-from spacy.symbols import dobj, nmod, amod, advmod, pobj, prep, NOUN, VERB
-import re
 
-from utils import write_jsonl, find_files
+from datatrove.pipeline.readers import IpcReader
+from datatrove.executor import LocalPipelineExecutor
+from datatrove.data import DocumentsPipeline
+from langdetect import detect_langs
+from datatrove.data import Document
+from datatrove.pipeline.filters.base_filter import BaseFilter
+from datatrove.pipeline.writers.disk_base import DiskWriter
+from datatrove.pipeline.writers.jsonl import JsonlWriter
 
-compound = 7037928807040764755
 
-DATA_DIR = "/mnt/u14157_ic_nlp_001_files_nfs/nlpdata1/share/datasets/allenai___json/en-139638ded39e9288/0.0.0/8bb11242116d547c741b2e8a1f18598ffdd40a1d4f2a2872c7a28b697434bc96"
+DATA_DIR = "/mnt/nlpdata1/share/datasets/allenai___c4/tr/0.0.0/1588ec454efa1a09f29cd18ddd04fe05fc8653a2"
+DUMP_DATA_DIR = "/mnt/nlpdata1/home/ismayilz/morph-gen-c4"
 
-nlp = spacy.load("en_core_web_lg", exclude=["ner"])
+def preprocessing_adapter(document: Document, source_file: str, id_in_file: str = None) -> DocumentsPipeline:
+    return {
+        "id": id_in_file,
+        "text": document["text"].lower(),
+        "metadata": {
+            "url": document["url"],
+            "timestamp": str(document["timestamp"]),
+            "file_stem": pathlib.Path(source_file).stem
+        }
+    }
 
-def process_sentence(sentence):
-    doc = nlp(sentence)
-    ngrams = []
+class TRLanguageFilter(BaseFilter):
+    name = "🌍 Language ID"
+    _requires_dependencies = ["langdetect"]
 
-    for token in doc:
-        if not token.is_stop:
-            if token.dep == dobj and token.pos == NOUN:
-                ngrams.append((token.head.lemma_.lower(), token.lemma_.lower()))
-            elif token.dep in [nmod, amod, advmod, compound]:
-                if token.head.pos in [NOUN, VERB]:
-                    ngrams.append((token.lemma_.lower(), token.head.lemma_.lower()))
-            elif token.dep == pobj and token.head and token.head.dep == prep and token.head.head:
-                ngrams.append((token.head.head.lemma_.lower(), token.head.lemma_.lower(), token.lemma_.lower()))
+    def __init__(
+        self,
+        language_threshold: float = 0.9,
+        exclusion_writer: DiskWriter = None,
+    ):
+        super().__init__(exclusion_writer)
+        self.language_threshold = language_threshold
+        self._model = None
 
-    return ngrams
+    def filter(self, doc: Document) -> bool:
+        tr_score = 0
 
-def process_sample(sample, source_file, sample_idx):
-    sample_data = {}
-    doc = nlp(sample["text"])
-    file_pattern = r"json-(?P<split>train|validation)-(?P<fnum>\d+)-of-(?P<mnum>\d+).arrow"
+        try:
+            lang_scores = detect_langs(doc.text)
 
-    match = re.fullmatch(file_pattern, source_file)
-    file_name = source_file
-
-    if match:
-        file_num = match.group("fnum")
-        max_num = match.group("mnum")
-        split = "val" if match.group("split") == "validation" else "train"
-        file_name = f"{split}-{file_num}-{max_num}"
-    
-    for sent_idx, sent in enumerate(doc.sents):
-        ngrams = [" ".join(ngram) for ngram in process_sentence(sent.text)]
+            for lang_score in lang_scores:
+                if lang_score.lang == "tr":
+                    tr_score = float(lang_score.prob)
+        except Exception as e:
+            pass
         
-        for ngram in ngrams:
-            if ngram not in sample_data:
-                sample_data[ngram] = {"count": 0, "sources": []}
-            sample_data[ngram]["count"] += 1
-            sample_data[ngram]["sources"].append(f"file={file_name},sample={sample_idx},sent={sent_idx}")
+        doc.metadata["tr_score"] = tr_score
+        return tr_score > self.language_threshold
     
-    return sample_data
-
-def merge_results(results):
-    merged_results = {}
-
-    for result in results:
-        for ngram, ngram_data in result.items():
-            if ngram not in merged_results:
-                merged_results[ngram] = {"count": 0, "sources": []}
-            merged_results[ngram]["count"] += ngram_data["count"]
-            merged_results[ngram]["sources"].extend(ngram_data["sources"])
-    
-    return merged_results
-
-def process_c4_file(data_path, output_dir):
-    data_path = pathlib.Path(data_path)
-    data_dir = str(data_path.parent)
-    data_file = str(data_path.name)
-    data = load_dataset(data_dir, data_files=data_file, streaming=True)
-    data = data["validation"] if "validation" in data else data["train"]
-    results = []
-    data_len = sum([1 for _ in iter(data)])
-
-    output_dir = pathlib.Path(f"{output_dir}/results/results_{data_path.stem}")
-    output_dir.mkdir(parents=True, exist_ok=True)
-    process_status_file = open(output_dir / "process_status.txt", "w", encoding="utf-8")
-
-    for sample_idx, sample in tqdm(enumerate(iter(data)), total=data_len, leave=True, position=0, desc=f"Processing C4 file: {data_file}", file=process_status_file):
-        result = process_sample(sample, data_file, sample_idx)
-        results.append(result)
-    
-    process_status_file.write("\nProcessing done.")
-    process_status_file.close()
-
-    merged_results = merge_results(results)
-
-    batch_size = 10000
-    num_batches = (len(merged_results) // batch_size) + 1
-
-    merged_results = list(merged_results.items())
-
-    merge_status_file = open(output_dir / "merge_status.txt", "w", encoding="utf-8")
-
-    for i in tqdm(range(num_batches), total=num_batches, desc="Merging results", leave=True, position=0, file=merge_status_file):
-        start = i * batch_size
-        end = (i + 1) * batch_size
-        results_batch = merged_results[start:end]
-        if results_batch:
-            write_jsonl([{"ngram": ngram, **ngram_data} for ngram, ngram_data in results_batch], output_dir / f"results_{data_path.stem}-batch{i}.jsonl")
-
-    merge_status_file.write("\nMerging done.")
-    merge_status_file.close()
-
-def process_c4_batch(data_paths, output_dir, batch_num=0):
-    status_dir = pathlib.Path(f"{output_dir}/status")
-    status_dir.mkdir(parents=True, exist_ok=True)
-    batch_status_file = open(status_dir / f"batch{batch_num}_status.txt", "w", encoding="utf-8")
-    
-    for data_path in tqdm(data_paths, total=len(data_paths), desc=f"Processing C4 batch {batch_num}", leave=True, position=0, file=batch_status_file):
-        process_c4_file(data_path, output_dir)
-
-def process_c4(data_paths, output_dir):
-    num_cores = multiprocessing.cpu_count()
-    num_cores = num_cores - int(num_cores * 0.10)
-    batch_size = (len(data_paths) // num_cores) + 1
-
-    procs = []
-
-    for i in range(num_cores):
-        start = i * batch_size
-        end = (i + 1) * batch_size
-        data_paths_batch = data_paths[start:end]
-        
-        if data_paths_batch:
-            p = multiprocessing.Process(target=process_c4_batch, args=(data_paths_batch, output_dir, i))
-            p.start()
-            procs.append(p)
-    
-    for p in procs:
-        p.join()
-
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--data-dir", type=str, default=DATA_DIR)
-    parser.add_argument("-o", "--output-dir", type=str, default="data")
-
-    args = parser.parse_args()
-
-    data_paths = find_files(args.data_dir, extension=".arrow")
-    process_c4(data_paths, args.output_dir)
+preprocessing = LocalPipelineExecutor(
+    pipeline=[
+        IpcReader(DATA_DIR, stream=True, progress=True, glob_pattern="*.arrow", adapter=preprocessing_adapter),
+        TRLanguageFilter(exclusion_writer=JsonlWriter(f"{DUMP_DATA_DIR}/lang_filter_removed", "${file_stem}_${rank}.jsonl")),
+        JsonlWriter(f"{DUMP_DATA_DIR}/lang_filtered", "${file_stem}_${rank}.jsonl")
+    ],
+    logging_dir=f"{DUMP_DATA_DIR}/logs/",
+    tasks=600,
+    workers=64
+)
 
 if __name__ == "__main__":
-    main()
+    preprocessing.run()
