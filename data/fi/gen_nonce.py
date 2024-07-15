bad829345312e2f3ed6bd0b8953db81e03a6488d
Jonne Sälevä <jonne@jonnesaleva.com>
2024-07-15 00:06:41 -0400

    Small tweaks to how the model samples things so we avoid things like triple vowels in a word

--- data/fi/gen_nonce.py
+++ data/fi/gen_nonce.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 
 import random
+import re
+
+import click
 
 
 def generate_finnish_word():
     front_vowels = {"i", "ä", "ö", "y"}
     back_vowels = {"u", "a", "o"}
     neutral_vowels = {"e"}
-
     vowels = list(front_vowels | back_vowels | neutral_vowels)
 
     neutral_diphthongs = [
@@ -40,27 +42,27 @@ def generate_finnish_word():
         "uu",
     ]
     consonants = "ktpnsmlrhjv"
-    syllable_patterns = ["vv", "kv", "vk", "kvk"]
+    syllable_patterns = ["v", "vv", "kv", "vk", "kvk"]
+    syllable_pattern_weights = [0.1, 0.2, 0.2, 0.4, 0.1]
 
     word_syllables = []
-    syllable_count = random.choice([2, 3, 4, 5])
+    syllable_count = random.choice([2, 3, 4])
+
+    vowel_type = random.choice(["front", "back", "neutral"])
 
     for syllable_id in range(syllable_count):
-        patterns_to_choose_from = (
-            syllable_patterns if syllable_id != syllable_count - 1 else ["vv", "kv"]
-        )
-        pattern = random.choice(patterns_to_choose_from)
+
+        if syllable_id != syllable_count - 1:
+            patterns_to_choose_from = syllable_patterns
+            syllable_pattern_weights = syllable_pattern_weights
+        else:
+            patterns_to_choose_from = ["k", "v"]
+            syllable_pattern_weights = [0.5, 0.5]
+
+        pattern = random.choices(
+            patterns_to_choose_from, weights=syllable_pattern_weights, k=1
+        )[0]
         syllable = ""
-
-        if "v" in pattern:
-            random_vowel = random.choice(vowels)
-
-            if random_vowel in front_vowels:
-                vowel_type = "front"
-            elif random_vowel in back_vowels:
-                vowel_type = "back"
-            else:
-                vowel_type = "neutral"
 
         for char in pattern:
             if char == "v":
@@ -73,6 +75,23 @@ def generate_finnish_word():
                     }[vowel_type]
                 )
 
+                # cannot choose sam vowel as previous syllable ends with
+
+                if syllable_id > 0:
+                    remove_this = word_syllables[syllable_id - 1][-1]
+                    vowels_to_choose_from = [
+                        v for v in vowels_to_choose_from if v != remove_this
+                    ]
+
+                    if len(vowels_to_choose_from) == 0:
+                        vowels_to_choose_from = list(
+                            {
+                                "front": front_vowels,
+                                "back": back_vowels,
+                                "neutral": neutral_vowels,
+                            }[vowel_type]
+                        )
+
                 diphthongs_to_choose_from = list(
                     {
                         "front": front_diphthongs,
@@ -81,19 +100,33 @@ def generate_finnish_word():
                     }[vowel_type]
                 )
 
-                if random.random() < 0.15:
+                if random.random() < 0.1:
                     syllable += random.choice(diphthongs_to_choose_from)
                 else:
                     syllable += random.choice(vowels_to_choose_from)
             else:
                 syllable += random.choice(consonants)
+
         word_syllables.append(syllable)
 
-    return "".join(word_syllables)
+    candidate_word = "".join(word_syllables)
+
+    # if word contains triple vowel, redo the call to this function
+    conditions = [re.search(r"[aeiouyäö]{3}", candidate_word)]
+
+    if any(conditions):
+        return generate_finnish_word()
+
+    else:
+        return candidate_word
 
 
-# Test the function
+@click.command()
+@click.option("--n-words", "-n", type=int, default=10)
+def main(n_words):
+    for _ in range(n_words):
+        print(generate_finnish_word())
+
 
 if __name__ == "__main__":
-    for _ in range(10):
-        print(generate_finnish_word())
+    main()
