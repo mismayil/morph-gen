ed78af10adcdbfa6ca5de7f85968a51e6e3f45e5
Mete Ismayilzada <mismayilza@gmail.com>
2024-08-28 19:40:20 +0200

    add early tr zs cot experiment results

--- src/evaluate_gpt.sh
+++ src/evaluate_gpt.sh
@@ -7,11 +7,12 @@ api_key=${4}
 data_dir="../experiments/data"
 outputs_dir="../experiments/outputs"
 
-for jsonfile in ${data_dir}/tr/btwd/final/eval/temp_en/${input_dir}/batch1/*.json
-do
-    echo "Evaluating ${jsonfile}"
-    python evaluate_lm.py -d ${jsonfile} -o ${outputs_dir}/${model}/tr/btwd/temp_en/final/${experiment}/batch1 -m ${model} -b 4 -oa
-done
+# tr default experiments
+# for jsonfile in ${data_dir}/tr/btwd/final/eval/temp_en/${input_dir}/batch1/*.json
+# do
+#     echo "Evaluating ${jsonfile}"
+#     python evaluate_lm.py -d ${jsonfile} -o ${outputs_dir}/${model}/tr/btwd/temp_en/final/${experiment}/batch1 -m ${model} -b 4 -oa
+# done
 
 # for jsonfile in ${data_dir}/tr/btwd/final/eval/temp_tr/${input_dir}/batch1/*.json
 # do
@@ -19,46 +20,14 @@ done
 #     python evaluate_lm.py -d ${jsonfile} -o ${outputs_dir}/${model}/tr/btwd/temp_tr/final/${experiment}/batch1 -m ${model} -b 4
 # done
 
-# for jsonfile in ${data_dir}/tr/btwd/test/eval/temp_tr/${input_dir}/*.json
-# do
-#     echo "Evaluating ${jsonfile}"
-#     python evaluate_lm.py -d ${jsonfile} -o ${outputs_dir}/${model}/tr/btwd/temp_tr/test/${experiment} -m ${model} -ia -b 4 -t 0.3
-# done
-# cot experiments
-# for jsonfile in ${data_dir}/tr/btwd/final/eval/temp_en/${input_dir}/*.json
-# do
-#     echo "Evaluating ${jsonfile}"
-#     python evaluate_lm.py -d ${jsonfile} -o ${outputs_dir}/${model}/tr/btwd/temp_en/final/${experiment} -m ${model} -ia -b 1 --max-tokens None
-# done
+# tr cot experiments
+for jsonfile in ${data_dir}/tr/btwd/final/eval/temp_en/${input_dir}/batch1/*.json
+do
+    echo "Evaluating ${jsonfile}"
+    python evaluate_lm.py -d ${jsonfile} -o ${outputs_dir}/${model}/tr/btwd/temp_en/final/${experiment}/batch1 -m ${model} -b 2 -oa -g None
+done
 
-# for jsonfile in ${data_dir}/tr/sense/eval/temp_en/${input_dir}/*.json
-# do
-#     if [[ $jsonfile == *"nonce"* ]]; then
-#         echo "Evaluating ${jsonfile}"
-#         python evaluate_lm.py -d ${jsonfile} -o ${outputs_dir}/${model}/tr/sense/temp_en/${experiment} -k ${openai_api_key} -m ${model} -ia
-#     fi
-# done
-
-# for jsonfile in ${data_dir}/tr/sense/eval/temp_tr/${input_dir}/*.json
-# do
-#     if [[ $jsonfile == *"nonce"* ]]; then
-#         echo "Evaluating ${jsonfile}"
-#         python evaluate_lm.py -d ${jsonfile} -o ${outputs_dir}/${model}/tr/sense/temp_tr/${experiment} -k ${openai_api_key} -m ${model} -ia
-#     fi
-# done
-
-# for jsonfile in ${data_dir}/en/morpholex/eval/temp_en/${input_dir}/*.json
-# do
-#     echo "Evaluating ${jsonfile}"
-#     python evaluate_lm.py -d ${jsonfile} -o ${outputs_dir}/${model}/en/morpholex/temp_en/${experiment} -k ${openai_api_key} -m ${model} -ia
-# done
-
-# for jsonfile in ${data_dir}/en/morpholex/eval/temp_tr/${input_dir}/*.json
-# do
-#     echo "Evaluating ${jsonfile}"
-#     python evaluate_lm.py -d ${jsonfile} -o ${outputs_dir}/${model}/en/morpholex/temp_tr/${experiment} -k ${openai_api_key} -m ${model} -ia
-# done
-
+# fi default experiments
 # for jsonfile in ${data_dir}/fi/final/eval/temp_en/${input_dir}/*.json
 # do
 #     echo "Evaluating ${jsonfile}"
