69d9d6ee134066d25ee753d5005f57b133aeb5c9
Mete Ismayilzada <mismayilza@gmail.com>
2024-08-30 16:40:58 +0200

    add tr temp_tr cot results

--- src/evaluate_gpt.sh
+++ src/evaluate_gpt.sh
@@ -7,27 +7,35 @@ api_key=${4}
 data_dir="../experiments/data"
 outputs_dir="../experiments/outputs"
 
-# tr default experiments
+# tr default experiments (temp_en)
 # for jsonfile in ${data_dir}/tr/btwd/final/eval/temp_en/${input_dir}/batch1/*.json
 # do
 #     echo "Evaluating ${jsonfile}"
 #     python evaluate_lm.py -d ${jsonfile} -o ${outputs_dir}/${model}/tr/btwd/temp_en/final/${experiment}/batch1 -m ${model} -b 4 -oa
 # done
 
+# tr default experiments (temp_tr)
 # for jsonfile in ${data_dir}/tr/btwd/final/eval/temp_tr/${input_dir}/batch1/*.json
 # do
 #     echo "Evaluating ${jsonfile}"
 #     python evaluate_lm.py -d ${jsonfile} -o ${outputs_dir}/${model}/tr/btwd/temp_tr/final/${experiment}/batch1 -m ${model} -b 4
 # done
 
-# tr cot experiments
-for jsonfile in ${data_dir}/tr/btwd/final/eval/temp_en/${input_dir}/batch1/*.json
+# tr cot experiments (temp_en)
+# for jsonfile in ${data_dir}/tr/btwd/final/eval/temp_en/${input_dir}/batch1/*.json
+# do
+#     echo "Evaluating ${jsonfile}"
+#     python evaluate_lm.py -d ${jsonfile} -o ${outputs_dir}/${model}/tr/btwd/temp_en/final/${experiment}/batch1 -m ${model} -b 4 -oa -g None
+# done
+
+# tr cot experiments (temp_tr)
+for jsonfile in ${data_dir}/tr/btwd/final/eval/temp_tr/${input_dir}/batch1/*.json
 do
     echo "Evaluating ${jsonfile}"
-    python evaluate_lm.py -d ${jsonfile} -o ${outputs_dir}/${model}/tr/btwd/temp_en/final/${experiment}/batch1 -m ${model} -b 2 -oa -g None
+    python evaluate_lm.py -d ${jsonfile} -o ${outputs_dir}/${model}/tr/btwd/temp_tr/final/${experiment}/batch1 -m ${model} -b 4 -oa -g None
 done
 
-# fi default experiments
+# fi default experiments (temp_en)
 # for jsonfile in ${data_dir}/fi/final/eval/temp_en/${input_dir}/*.json
 # do
 #     echo "Evaluating ${jsonfile}"
