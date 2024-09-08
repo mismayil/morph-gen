31992bb38f1352a6c1262a8b6072377af119cdc3
Mete Ismayilzada <mismayilza@gmail.com>
2024-09-08 19:56:15 +0200

    add gemini fi results

--- src/evaluate_gemini.sh
+++ src/evaluate_gemini.sh
@@ -7,16 +7,30 @@ api_key=${4}
 data_dir="../experiments/data"
 outputs_dir="../experiments/outputs"
 
-# tr default experiments (temp_en)
-for jsonfile in ${data_dir}/tr/btwd/final/eval/temp_en/${input_dir}/batch1/*.json
+# # tr default experiments (temp_en)
+# for jsonfile in ${data_dir}/tr/btwd/final/eval/temp_en/${input_dir}/batch1/*.json
+# do
+#     echo "Evaluating ${jsonfile}"
+#     python evaluate_lm.py -d ${jsonfile} -o ${outputs_dir}/${model}/tr/btwd/temp_en/final/${experiment}/batch1 -m ${model} -b 8
+# done
+
+# # tr default experiments (temp_tr)
+# for jsonfile in ${data_dir}/tr/btwd/final/eval/temp_tr/${input_dir}/batch1/*.json
+# do
+#     echo "Evaluating ${jsonfile}"
+#     python evaluate_lm.py -d ${jsonfile} -o ${outputs_dir}/${model}/tr/btwd/temp_tr/final/${experiment}/batch1 -m ${model} -b 8
+# done
+
+# fi default experiments (temp_en)
+for jsonfile in ${data_dir}/fi/final/eval/temp_en/${input_dir}/*.json
 do
     echo "Evaluating ${jsonfile}"
-    python evaluate_lm.py -d ${jsonfile} -o ${outputs_dir}/${model}/tr/btwd/temp_en/final/${experiment}/batch1 -m ${model} -b 8
+    python evaluate_lm.py -d ${jsonfile} -o ${outputs_dir}/${model}/fi/temp_en/final/${experiment} -m ${model} -b 8
 done
 
-# tr default experiments (temp_tr)
-for jsonfile in ${data_dir}/tr/btwd/final/eval/temp_tr/${input_dir}/batch1/*.json
+# fi default experiments (temp_fi)
+for jsonfile in ${data_dir}/fi/final/eval/temp_fi/${input_dir}/*.json
 do
     echo "Evaluating ${jsonfile}"
-    python evaluate_lm.py -d ${jsonfile} -o ${outputs_dir}/${model}/tr/btwd/temp_tr/final/${experiment}/batch1 -m ${model} -b 8
+    python evaluate_lm.py -d ${jsonfile} -o ${outputs_dir}/${model}/fi/temp_fi/final/${experiment} -m ${model} -b 8
 done
