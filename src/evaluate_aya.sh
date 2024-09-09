f5790d75fe746f455964f4eb17d4643fadfbeda0
Mete <mismayilza@gmail.com>
2024-09-09 10:42:21 +0000

    add new aya-8b and tr-llama-8b results

--- src/evaluate_aya.sh
+++ src/evaluate_aya.sh
@@ -23,14 +23,14 @@ do
 done
 
 # TR default experiments (temp_tr)
-# for jsonfile in ${data_dir}/tr/btwd/final/eval/temp_tr/${input_dir}/batch1/*.json
-# do
-#     echo "Evaluating ${jsonfile}"
-#     python evaluate_lm.py -d ${jsonfile} \
-#                           -o ${outputs_dir}/${model}/tr/btwd/temp_tr/final/${experiment}/batch1 \
-#                           -m ${model} \
-#                           -b 4 \
-#                           -mp ${model_path} \
-#                           -tp ${tokenizer_path} \
-#                           -c ${cache_dir}
-# done
+for jsonfile in ${data_dir}/tr/btwd/final/eval/temp_tr/${input_dir}/batch1/*.json
+do
+    echo "Evaluating ${jsonfile}"
+    python evaluate_lm.py -d ${jsonfile} \
+                          -o ${outputs_dir}/${model}/tr/btwd/temp_tr/final/${experiment}/batch1 \
+                          -m ${model} \
+                          -b 4 \
+                          -mp ${model_path} \
+                          -tp ${tokenizer_path} \
+                          -c ${cache_dir}
+done
--- src/evaluate_llama.sh
+++ src/evaluate_llama.sh
@@ -10,27 +10,27 @@ data_dir="../experiments/data"
 outputs_dir="../experiments/outputs"
 
 # tr temp_en experiments
-for jsonfile in ${data_dir}/tr/btwd/final/eval/temp_en/${input_dir}/batch1/*.json
-do
-    echo "Evaluating ${jsonfile}"
-    python evaluate_lm.py -d ${jsonfile} \
-                          -o ${outputs_dir}/${model}/tr/btwd/temp_en/final/${experiment}/batch1 \
-                          -m ${model} \
-                          -b 4 \
-                          -mp ${model_path} \
-                          -tp ${tokenizer_path} \
-                          -c ${cache_dir}
-done
-
-# tr temp_tr experiments
-# for jsonfile in ${data_dir}/tr/btwd/final/eval/temp_tr/${input_dir}/batch1/*.json
+# for jsonfile in ${data_dir}/tr/btwd/final/eval/temp_en/${input_dir}/batch1/*.json
 # do
 #     echo "Evaluating ${jsonfile}"
 #     python evaluate_lm.py -d ${jsonfile} \
-#                           -o ${outputs_dir}/${model}/tr/btwd/temp_tr/final/${experiment}/batch1 \
+#                           -o ${outputs_dir}/${model}/tr/btwd/temp_en/final/${experiment}/batch1 \
 #                           -m ${model} \
 #                           -b 4 \
 #                           -mp ${model_path} \
 #                           -tp ${tokenizer_path} \
 #                           -c ${cache_dir}
 # done
+
+# tr temp_tr experiments
+for jsonfile in ${data_dir}/tr/btwd/final/eval/temp_tr/${input_dir}/batch1/*.json
+do
+    echo "Evaluating ${jsonfile}"
+    python evaluate_lm.py -d ${jsonfile} \
+                          -o ${outputs_dir}/${model}/tr/btwd/temp_tr/final/${experiment}/batch1 \
+                          -m ${model} \
+                          -b 4 \
+                          -mp ${model_path} \
+                          -tp ${tokenizer_path} \
+                          -c ${cache_dir}
+done
