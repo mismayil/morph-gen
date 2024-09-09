a98c2fb1028f6df0c9ec17841d636feac2b8d1bb
Mete <mismayilza@gmail.com>
2024-09-09 23:09:22 +0000

    add new results

--- src/evaluate_aya.sh
+++ src/evaluate_aya.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 experiment=${1:-"v_final_default"}
-model=${2:-"aya-23-8b"}
-model_path=${3:-"CohereForAI/aya-23-8b"}
-tokenizer_path=${3:-"CohereForAI/aya-23-8b"}
+model=${2:-"aya-23-35b"}
+model_path=${3:-"CohereForAI/aya-23-35b"}
+tokenizer_path=${3:-"CohereForAI/aya-23-35b"}
 cache_dir=${4:-"/mnt/scratch/home/ismayilz/.cache/huggingface/hub"}
 input_dir=${5:-${experiment}}
 data_dir="../experiments/data"
@@ -23,14 +23,14 @@ do
 done
 
 # TR default experiments (temp_tr)
-for jsonfile in ${data_dir}/tr/btwd/final/eval/temp_tr/${input_dir}/batch1/*.json
-do
-    echo "Evaluating ${jsonfile}"
-    python evaluate_lm.py -d ${jsonfile} \
-                          -o ${outputs_dir}/${model}/tr/btwd/temp_tr/final/${experiment}/batch1 \
-                          -m ${model} \
-                          -b 4 \
-                          -mp ${model_path} \
-                          -tp ${tokenizer_path} \
-                          -c ${cache_dir}
-done
+# for jsonfile in ${data_dir}/tr/btwd/final/eval/temp_tr/${input_dir}/batch1/*.json
+# do
+#     echo "Evaluating ${jsonfile}"
+#     python evaluate_lm.py -d ${jsonfile} \
+#                           -o ${outputs_dir}/${model}/tr/btwd/temp_tr/final/${experiment}/batch1 \
+#                           -m ${model} \
+#                           -b 4 \
+#                           -mp ${model_path} \
+#                           -tp ${tokenizer_path} \
+#                           -c ${cache_dir}
+# done
