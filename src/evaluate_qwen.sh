dba9f3f36e344cca67b41ba202f14168a928eed9
Mete <mismayilza@gmail.com>
2024-10-12 17:17:34 +0000

    add new qwen tr results

--- src/evaluate_qwen.sh
+++ src/evaluate_qwen.sh
@@ -10,18 +10,18 @@ input_dir=${5:-${experiment}}
 data_dir="../experiments/data"
 outputs_dir="../experiments/outputs"
 
-# FI default experiments (temp_en)
-for jsonfile in ${data_dir}/fi/v4/eval/temp_en/${input_dir}/*.json
-do
-    echo "Evaluating ${jsonfile}"
-    python evaluate_lm.py -d ${jsonfile} \
-                          -o ${outputs_dir}/${model}/fi/temp_en/v4/${experiment} \
-                          -m ${model} \
-                          -b 4 \
-                          -mp ${model_path} \
-                          -tp ${tokenizer_path} \
-                          -c ${cache_dir}
-done
+# # FI default experiments (temp_en)
+# for jsonfile in ${data_dir}/fi/v4/eval/temp_en/${input_dir}/*.json
+# do
+#     echo "Evaluating ${jsonfile}"
+#     python evaluate_lm.py -d ${jsonfile} \
+#                           -o ${outputs_dir}/${model}/fi/temp_en/v4/${experiment} \
+#                           -m ${model} \
+#                           -b 4 \
+#                           -mp ${model_path} \
+#                           -tp ${tokenizer_path} \
+#                           -c ${cache_dir}
+# done
 
 # # FI default experiments (temp_fi)
 # for jsonfile in ${data_dir}/fi/v4/eval/temp_fi/${input_dir}/*.json
@@ -36,3 +36,29 @@ done
 #                           -c ${cache_dir} \
 #                           --resume
 # done
+
+# TR default experiments (temp_en)
+for jsonfile in ${data_dir}/tr/btwd/final/eval/temp_en/${input_dir}/batch1/*.json
+do
+    echo "Evaluating ${jsonfile}"
+    python evaluate_lm.py -d ${jsonfile} \
+                          -o ${outputs_dir}/${model}/tr/btwd/temp_en/final/${experiment}/batch1 \
+                          -m ${model} \
+                          -b 4 \
+                          -mp ${model_path} \
+                          -tp ${tokenizer_path} \
+                          -c ${cache_dir}
+done
+
+# TR default experiments (temp_tr)
+for jsonfile in ${data_dir}/tr/btwd/final/eval/temp_tr/${input_dir}/batch1/*.json
+do
+    echo "Evaluating ${jsonfile}"
+    python evaluate_lm.py -d ${jsonfile} \
+                          -o ${outputs_dir}/${model}/tr/btwd/temp_tr/final/${experiment}/batch1 \
+                          -m ${model} \
+                          -b 4 \
+                          -mp ${model_path} \
+                          -tp ${tokenizer_path} \
+                          -c ${cache_dir}
+done
