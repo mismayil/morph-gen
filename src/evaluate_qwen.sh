9536326407f482e317090480ea0a14789ef3a05b
Mete <mismayilza@gmail.com>
2024-10-13 16:34:11 +0000

    add new qwen tr results

--- src/evaluate_qwen.sh
+++ src/evaluate_qwen.sh
@@ -38,27 +38,28 @@ outputs_dir="../experiments/outputs"
 # done
 
 # TR default experiments (temp_en)
-for jsonfile in ${data_dir}/tr/btwd/final/eval/temp_en/${input_dir}/batch1/*.json
-do
-    echo "Evaluating ${jsonfile}"
-    python evaluate_lm.py -d ${jsonfile} \
-                          -o ${outputs_dir}/${model}/tr/btwd/temp_en/final/${experiment}/batch1 \
-                          -m ${model} \
-                          -b 4 \
-                          -mp ${model_path} \
-                          -tp ${tokenizer_path} \
-                          -c ${cache_dir}
-done
+# for jsonfile in ${data_dir}/tr/btwd/final/eval/temp_en/${input_dir}/batch1/*.json
+# do
+#     echo "Evaluating ${jsonfile}"
+#     python evaluate_lm.py -d ${jsonfile} \
+#                           -o ${outputs_dir}/${model}/tr/btwd/temp_en/final/${experiment}/batch1 \
+#                           -m ${model} \
+#                           -b 4 \
+#                           -mp ${model_path} \
+#                           -tp ${tokenizer_path} \
+#                           -c ${cache_dir}
+# done
 
 # TR default experiments (temp_tr)
-for jsonfile in ${data_dir}/tr/btwd/final/eval/temp_tr/${input_dir}/batch1/*.json
-do
-    echo "Evaluating ${jsonfile}"
-    python evaluate_lm.py -d ${jsonfile} \
-                          -o ${outputs_dir}/${model}/tr/btwd/temp_tr/final/${experiment}/batch1 \
-                          -m ${model} \
-                          -b 4 \
-                          -mp ${model_path} \
-                          -tp ${tokenizer_path} \
-                          -c ${cache_dir}
-done
+# for jsonfile in ${data_dir}/tr/btwd/final/eval/temp_tr/${input_dir}/batch1/*.json
+# do
+#     echo "Evaluating ${jsonfile}"
+#     python evaluate_lm.py -d ${jsonfile} \
+#                           -o ${outputs_dir}/${model}/tr/btwd/temp_tr/final/${experiment}/batch1 \
+#                           -m ${model} \
+#                           -b 4 \
+#                           -mp ${model_path} \
+#                           -tp ${tokenizer_path} \
+#                           -c ${cache_dir}
+# done
+
