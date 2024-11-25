3741d0ddb874530362c8240d3041c349703c0ec6
Mete Ismayilzada <mismayilza@gmail.com>
2024-11-25 02:32:51 +0100

    add alt experiment results

--- src/evaluate_gpt.sh
+++ src/evaluate_gpt.sh
@@ -8,11 +8,11 @@ data_dir="../experiments/data"
 outputs_dir="../experiments/outputs"
 
 # tr default experiments (temp_en)
-# for jsonfile in ${data_dir}/tr/btwd/final/eval/temp_en/${input_dir}/batch1/*.json
-# do
-#     echo "Evaluating ${jsonfile}"
-#     python evaluate_lm.py -d ${jsonfile} -o ${outputs_dir}/${model}/tr/btwd/temp_en/final/${experiment}/batch1 -m ${model} -b 4 -oa
-# done
+for jsonfile in ${data_dir}/tr/btwd/final/eval/temp_en/${input_dir}/batch1/*.json
+do
+    echo "Evaluating ${jsonfile}"
+    python evaluate_lm.py -d ${jsonfile} -o ${outputs_dir}/${model}/tr/btwd/temp_en/final/${experiment}/batch1 -m ${model} -b 8
+done
 
 # tr default experiments (temp_tr)
 # for jsonfile in ${data_dir}/tr/btwd/final/eval/temp_tr/${input_dir}/batch1/*.json
@@ -36,11 +36,11 @@ outputs_dir="../experiments/outputs"
 # done
 
 # fi default experiments (temp_en)
-# for jsonfile in ${data_dir}/fi/v4/eval/temp_en/${input_dir}/*.json
-# do
-#     echo "Evaluating ${jsonfile}"
-#     python evaluate_lm.py -d ${jsonfile} -o ${outputs_dir}/${model}/fi/temp_en/v4/${experiment} -m ${model} -b 8
-# done
+for jsonfile in ${data_dir}/fi/v4/eval/temp_en/${input_dir}/*.json
+do
+    echo "Evaluating ${jsonfile}"
+    python evaluate_lm.py -d ${jsonfile} -o ${outputs_dir}/${model}/fi/temp_en/v4/${experiment} -m ${model} -b 8
+done
 
 # # fi default experiments (temp_fi)
 # for jsonfile in ${data_dir}/fi/v4/eval/temp_fi/${input_dir}/*.json
@@ -85,24 +85,24 @@ outputs_dir="../experiments/outputs"
 #     done
 # done
 
-# fi default experiments (temp_en) with temperature sampling
-temperatures=(0.3 0.5 0.7 0.9)
-for temperature in ${temperatures[@]}
-do
-    for jsonfile in ${data_dir}/fi/v4/eval/temp_en/${input_dir}/*.json
-    do
-        echo "Evaluating ${jsonfile}"
-        python evaluate_lm.py -d ${jsonfile} -o ${outputs_dir}/${model}/fi/temp_en/v4/${experiment} -m ${model} -b 8 -t ${temperature} -p 1
-    done
-done
+# # fi default experiments (temp_en) with temperature sampling
+# temperatures=(0.3 0.5 0.7 0.9)
+# for temperature in ${temperatures[@]}
+# do
+#     for jsonfile in ${data_dir}/fi/v4/eval/temp_en/${input_dir}/*.json
+#     do
+#         echo "Evaluating ${jsonfile}"
+#         python evaluate_lm.py -d ${jsonfile} -o ${outputs_dir}/${model}/fi/temp_en/v4/${experiment}_t${temperature} -m ${model} -b 8 -t ${temperature} -p 1
+#     done
+# done
 
-# fi default experiments (temp_en) with top_p sampling
-top_ps=(0.9 0.95)
-for top_p in ${top_ps[@]}
-do
-    for jsonfile in ${data_dir}/fi/v4/eval/temp_en/${input_dir}/*.json
-    do
-        echo "Evaluating ${jsonfile}"
-        python evaluate_lm.py -d ${jsonfile} -o ${outputs_dir}/${model}/fi/temp_en/v4/${experiment} -m ${model} -b 8 -p ${top_p} -t 1
-    done
-done
+# # fi default experiments (temp_en) with top_p sampling
+# top_ps=(0.9 0.95)
+# for top_p in ${top_ps[@]}
+# do
+#     for jsonfile in ${data_dir}/fi/v4/eval/temp_en/${input_dir}/*.json
+#     do
+#         echo "Evaluating ${jsonfile}"
+#         python evaluate_lm.py -d ${jsonfile} -o ${outputs_dir}/${model}/fi/temp_en/v4/${experiment}_p${top_p} -m ${model} -b 8 -p ${top_p} -t 1
+#     done
+# done
